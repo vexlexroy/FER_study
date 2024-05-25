bf3574d2fb3c014bab3968e4a5f97e949d2e49c7
Leonardo Roy Sabolić <148395425+vexlexroy@users.noreply.github.com>
2024-05-25 15:21:56 +0200

    setup for Virtual labs

---
+++ Virtual/lab2/RayTracing_C#/RayTracing/RayTracing/Scene.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace raytracing
+{
+    /// <summary>
+    /// Klasa predstvlja scenu kod modela crtanja slike pomocu ray tracinga. Sastoji
+    /// se od izvora svjetlosti i konacnog broja objekata.
+    /// </summary>
+    public class Scene
+    {
+        const int MAXDEPTH = 5; //maksimalna dubina rekurzije
+        private int numberOfObjects;
+        private Sphere[] sphere;
+        private Point lightPosition;
+        private ColorVector backgroundColors = new ColorVector(0, 0, 0);
+        private ColorVector light = new ColorVector((float)1, (float)1, (float)1);
+        private ColorVector ambientLight = new ColorVector((float)0.5, (float)0.5, (float)0.5);
+
+        /// <summary>
+        /// Inicijalni konstruktor koji postavlja poziciju svijetla i parametre svih
+        /// objekata u sceni.
+        /// </summary>
+        /// <param name="lightPosition">pozicija svijetla</param>
+        /// <param name="numberOfObjects">broj objekata u sceni</param>
+        /// <param name="sphereParameters">parametri svih kugli</param>
+        public Scene ( Point lightPosition, int numberOfObjects, SphereParameters[] sphereParameters )
+        {
+            this.lightPosition = lightPosition;
+            this.numberOfObjects = numberOfObjects;
+            sphere = new Sphere[numberOfObjects];
+            for(int i = 0; i < numberOfObjects; i++)
+            {
+                sphere[i] = new Sphere(sphereParameters[i]);
+            }
+        }
+
+        /// <summary>
+        /// Metoda provjerava da li postoji sjena na tocki presjeka. Vraca true ako
+        /// se zraka od mjesta presjeka prema izvoru svjetlosti sjece s nekim objektom.
+        /// </summary>
+        /// <param name="intersection">tocka presjeka</param>
+        /// <returns>true ako postoji sjena u tocki presjeka, false ako ne postoji</returns>
+        private bool shadow ( Point intersection )
+        {
+            return false;
+        }
+
+        /// <summary>
+        /// Metoda koja pomocu pracenja zrake racuna boju u tocki presjeka. Racuna se
+        /// osvjetljenje u tocki presjeka te se zbraja s doprinosima osvjetljenja koje
+        /// donosi reflektirana i refraktirana zraka.
+        /// </summary>
+        /// <param name="ray">pracena zraka</param>
+        /// <param name="depth">dubina rekurzije</param>
+        /// <returns>vektor boje u tocki presjeka</returns>
+        public ColorVector traceRay ( Ray ray, int depth )
+        {
+            return null;
+        }
+    }
+}
---
+++ Virtual/lab2/RayTracing_C#/RayTracing/RayTracing/Sphere.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace raytracing
+{
+    /// <summary>
+    /// Klasa predstavlja kuglu u prostoru. Nasljeduje apstraktnu klasu Object. Kugla
+    /// je odredena svojim polozajem, radijusom, bojom, parametrima materijala i
+    /// udjelima pojedninih zraka (osnovne, odbijene i lomljene).
+    /// </summary>
+    public class Sphere:Object
+    {
+        private double radius;
+        const double Epsilon = 0.0001;
+        private Point IntersectionPoint;
+
+        /// <summary>
+        /// Inicijalni konstruktor koji postavlja sve parametre kugle. Za prijenos
+        /// parametara koristi se pomocna klasa SphereParameters.
+        /// </summary>
+        /// <param name="sphereParameters">parametri kugle</param>
+        public Sphere ( SphereParameters sphereParameters )
+            : base(sphereParameters.getCenterPosition(), sphereParameters.getRaysContributions(),
+                sphereParameters.getMaterialParameters(), sphereParameters.getN(),
+                sphereParameters.getNi())
+        {
+            this.radius = sphereParameters.getRadius();
+        }
+
+        /// <summary>
+        /// Metoda ispituje postojanje presjeka zrake ray s kuglom. Ako postoji presjek
+        /// postavlja tocku presjeka IntersectionPoint, te
+        /// vraca logicku vrijednost true.
+        /// </summary>
+        /// <param name="ray">zraka za koju se ispituje postojanje presjeka sa kuglom</param>
+        /// <returns>logicku vrijednost postojanja presjeka zrake s kuglom</returns>
+        public override bool intersection ( Ray ray )
+        {
+			return false;
+        }
+
+        /// <summary>
+        /// Vraca tocku presjeka kugle sa zrakom koja je bliza pocetnoj tocki zrake.
+        /// </summary>
+        /// <returns>tocka presjeka zrake s kuglom koja je bliza izvoru zrake</returns>
+        public override Point getIntersectionPoint ()
+        {
+            return IntersectionPoint;
+        }
+
+        /// <summary>
+        /// Vraca normalu na kugli u tocki point
+        /// </summary>
+        /// <param name="point">point na kojoj se racuna normala na kugli</param>
+        /// <returns>normal vektor normale</returns>
+        public override Vector getNormal ( Point point )
+        {
+            return null;
+        }
+    }
+}
