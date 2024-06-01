f88408d17bc97404cdeb71f48f2682ee4dbbc503
Leonardo Roy Sabolić <148395425+vexlexroy@users.noreply.github.com>
2024-06-01 12:43:41 +0200

    Ucitavanje podataka lab4 UUUI

---
+++ UUI/LAB4/0036538682/solution.py
@@ -0,0 +1,55 @@
+import sys
+import argparse
+import numpy
+
+
+
+
+# ucitavanje podataka
+def parse_in(args):
+    parser=argparse.ArgumentParser()
+    parser.add_argument("--train", nargs="?", help="train set putanja")
+    parser.add_argument("--test",nargs="?", help="test set putanja")
+    parser.add_argument("--nn", nargs="?",choices=['5s', '20s', '5s5s'], help="Arhitektura neuronske mreze")
+    parser.add_argument("--popsize", nargs="?", help="velicina populacije")
+    parser.add_argument("--elitism", nargs="?", help="elitizam gen algoritma")
+    parser.add_argument("--p", nargs="?", help="vjerojatnost mutacije svakog elementa")
+    parser.add_argument("--K", nargs="?", help="standardna devijacia gausov sum")
+    parser.add_argument("--iter", nargs="?", help="broj iteracija")
+    pars_args = parser.parse_args(args)
+    with open(pars_args.train, "r") as r:
+        all_lines_train = r.readlines()
+    with open(pars_args.test, "r") as r:
+        all_lines_test = r.readlines()
+
+    nn=pars_args.nn
+    pops=pars_args.popsize
+    elit=pars_args.elitism
+    p=pars_args.p
+    K=pars_args.K
+    itr=pars_args.iter
+    # popunjavanje tran i test dicta
+    tarain_set={}
+    test_set={}
+    for idn,i in enumerate(all_lines_train[0].replace("\n","").split(",")):
+            tarain_set[idn]=[]
+            test_set[idn]=[]
+
+    for i in all_lines_train[1:]:
+        for idn, j in enumerate(i.replace("\n","").split(",")):
+            tarain_set[idn].append(j)
+
+    for i in all_lines_test[1:]:
+        for idn, j in enumerate(i.replace("\n","").split(",")):
+            test_set[idn].append(j)
+
+    return tarain_set, test_set, nn, pops, elit, p, K, itr
+
+
+
+
+
+if __name__=='__main__':
+     print("start")
+     print(parse_in(sys.argv[1:]))
+     
