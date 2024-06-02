4ba4a3fe6c7ed26fce48d260a449bbec506c7435
Leonardo Roy Sabolić <148395425+vexlexroy@users.noreply.github.com>
2024-06-02 20:09:50 +0200

    lab4 uui nural net

--- UUI/LAB4/0036538682/solution.py
+++ UUI/LAB4/0036538682/solution.py
@@ -1,8 +1,55 @@
 import sys
 import argparse
-import numpy
+import numpy as np
+
+class NeuralNet:
+    layers={} # layer : weights(np array)
+    arcitecture:str
+    inParams:int
+    outParams:int
+    fit:float = None
+
+    def __init__(self, architecture:str=None, inparm:int=None, outparm:int=None, existingweights:dict=None):
+        if (existingweights==None):
+            self.arcitecture=architecture
+            self.inParams=inparm
+            self.outParams=outparm
+            arc=(str(self.inParams)+'s'+architecture+str(self.outParams)).split('s')
+            # print(architecture.split("s"))
+            # print(arc)
+            for i,x in enumerate(arc[1:]):
+                # print(i, " ", x)
+                self.layers[i]=np.random.uniform(low=-1.0, high=1.0, size=(int(arc[i]), int(arc[i+1])))
+        else:
+            self.layers=existingweights
+            self.inParams=inparm
+            self.outParams=outparm
+            pass
+        return
+    
+    def set_fit(self, fitnes:float):
+        self.fit=fitnes
+        return
+    
+    def get_fit(self):
+        return self.fit
+    
+    def sigmoid_finction(self, xexp):
+        return 1/(1 + np.exp(xexp))
+    
+    def calculate_resault(self, input:np.array):
+        out=input
+        for i,x in enumerate(self.layers):
+            if(i<len(self.layers)-1):
+                out=self.sigmoid_finction(np.dot(out,self.layers[x]))
+            else:
+                out=self.sigmoid_finction(np.dot(out,self.layers[x]))
+        self.fit=None
+        return out
+
 
 class Genetika:
+    populacija:list=[]
     train_set:dict
     test_set:dict
     nn:str
@@ -20,6 +67,29 @@ class Genetika:
         self.mut_prob=p
         self.noise=K
         self.iterate=itr
+        for i in range(int(self.popsize)):
+            self.populacija.append(NeuralNet(nn,len(train_set)-1,1))
+        return
+    
+    def Mutate(self):
+        # pick random num and add in range (K,-K)
+        # gusian noise?
+        pass
+    def CrosParents(self):
+        # generate children
+        # mutate children
+        # return child
+        pass
+    def NewPopulate(self):
+        new_population={}
+
+        pass
+
+    def calculateFitnes():
+        pass
+
+
+
 
     
 
@@ -29,7 +99,7 @@ def parse_in(args):
     parser=argparse.ArgumentParser()
     parser.add_argument("--train", nargs="?", help="train set putanja")
     parser.add_argument("--test",nargs="?", help="test set putanja")
-    parser.add_argument("--nn", nargs="?",choices=['5s', '20s', '5s5s'], help="Arhitektura neuronske mreze")
+    parser.add_argument("--nn", nargs="?", help="Arhitektura neuronske mreze")
     parser.add_argument("--popsize", nargs="?", help="velicina populacije")
     parser.add_argument("--elitism", nargs="?", help="elitizam gen algoritma")
     parser.add_argument("--p", nargs="?", help="vjerojatnost mutacije svakog elementa")
@@ -71,5 +141,8 @@ def parse_in(args):
 if __name__=='__main__':
     #  print("start")
     #  print(parse_in(sys.argv[1:]))
-    gen = Genetika(parse_in(sys.argv[1:]))
+    NeuralNet.calculate_resault
+    gen = Genetika(*parse_in(sys.argv[1:]))
+    print(gen.populacija[0].layers)
+    print(gen.populacija[0].calculate_resault(np.array([3.469])))
 
